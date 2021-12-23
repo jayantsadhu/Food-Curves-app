dcff322221ca1a674d16acaba91b9c8b8d98643f
jayantsadhu <jayantsadhu6@gmail.com>
2021-12-24 03:41:39 +0530

    Login Activity xml

--- app/src/main/java/com/example/foodcurves/IntroductoryActivity.java
+++ app/src/main/java/com/example/foodcurves/IntroductoryActivity.java
@@ -1,8 +1,15 @@
 package com.example.foodcurves;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -11,6 +18,13 @@ public class IntroductoryActivity extends AppCompatActivity {
 
     ImageView logo, appName, splashImg;
     LottieAnimationView lottieAnimationView;
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+
+    Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +38,41 @@ public class IntroductoryActivity extends AppCompatActivity {
         splashImg.animate().translationY(-3000).setDuration(1000).setStartDelay(4000);
         logo.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
         appName.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
-        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
+
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        animation = AnimationUtils.loadAnimation(this,R.anim.splash_fading);
+        viewPager.startAnimation(animation);
+    }
+    public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
---
+++ app/src/main/java/com/example/foodcurves/OnBoardingFragment2.java
@@ -0,0 +1,16 @@
+package com.example.foodcurves;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class OnBoardingFragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_on_boarding2,container, false);
+        return view;
+    }
+}
