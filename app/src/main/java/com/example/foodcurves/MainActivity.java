ab392bcbed92fa81de024f4586d4f8f6b8ecfa5f
jayant <jayantsadhu6@gmail.com>
2022-08-29 23:39:38 +0530

    Dynamic menu item testing

--- app/src/main/java/com/example/foodcurves/MainActivity.java
+++ app/src/main/java/com/example/foodcurves/MainActivity.java
@@ -8,10 +8,12 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 
 import com.example.foodcurves.loginactivity.LoginActivity;
